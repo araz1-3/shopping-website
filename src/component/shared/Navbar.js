88be192154cc68f3b007827b4b0974a068e4ca13
Araz <alirezamahmoodi192@gmail.com>
2022-05-19 22:08:37 +0430

    navbar updated

--- src/component/shared/Navbar.js
+++ src/component/shared/Navbar.js
@@ -74,8 +74,16 @@ const Navbar = () => {
                    : "flex fixed top-4 right-2 md:static transition-all ease-linear duration-300 gap-8"
                }
                >
-                   <NavIcon state={FavState.favoriteCounter} path="/favorites" navbar={navbar} />
-                   <NavIcon state={state.itemsCenter} path="/cart" navbar={navbar} />
+                   <NavIcon
+                       state={FavState.favoriteCounter}
+                       path="/favorites" navbar={navbar}
+                       d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                   />
+                   <NavIcon
+                       state={state.itemsCenter}
+                       path="/cart" navbar={navbar}
+                       d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                   />
                </div>
             </div>
         </nav>
